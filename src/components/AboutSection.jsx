f2e9cbce547a3629f1f6af63c56ed02eb50967ff
Muhammed Hany <muuhamedhany@gmail.com>
2025-09-21 12:06:13 +0300

    change CV drive Link

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ const AboutSection = () => {
 
               <a
                 target="_blank"
-                href="https://drive.google.com/file/d/1HVbCDKFswfx9LJDpx9JMnK99lbfJiXmM/view?usp=sharing"
+                href="https://drive.google.com/file/d/1mi9KODE3nbwV_Vnk2mYG3GjWMH6yk9bc/view"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
